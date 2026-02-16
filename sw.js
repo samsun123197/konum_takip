bc7505306a3c90949c2757bde61fc774b76fd8b3
samsun123197 <54448019+samsun123197@users.noreply.github.com>
2026-02-16 13:34:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'konum-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// Kurulum ve Dosyaları Önbelleğe Alma
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Çevrimdışı Çalıştırma Stratejisi
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
